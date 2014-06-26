46a88dcb9a5ab220ee682ddbad410f808e4e788c
Ruslan <ruslan.n.kornienko@gmail.com>
2014-06-26 14:02:34 +0300

    Add mouseIn, mouseOut on delete button

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-$('.deleteAction').click(function() {
+  $('.deleteAction').hover( function (){
+    $(this).css('opacity', '1');
+  }, function (){
+    $(this).css('opacity', '0.5');
+  }
+  ).click(function() {
     var current_record = $(this).parents('tr');
       $.ajax({
         url: '/records/' + $(current_record).attr('data-record_id'),
